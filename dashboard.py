62a379b3dc74b4fc072c54c8551c85acf5b0ed57
theodoreellis <theodoreellis@gmail.com>
2020-12-05 20:44:43 -0800

    added quarter end values

--- dashboard.py
+++ dashboard.py
@@ -17,9 +17,9 @@ def today_value(series):
     today_end_date = datetime.today()
     today_start_date = buffer(today_end_date)
 
-    last_month_end_date = datetime.today().replace(day=1)
-    last_month_end_date = last_month_end_date - timedelta(days=1)
-    last_month_start_date = buffer(last_month_end_date)
+    #last_month_end_date = datetime.today().replace(day=1)
+    #last_month_end_date = last_month_end_date - timedelta(days=1)
+    #last_month_start_date = buffer(last_month_end_date)
 
 
     payload = {
@@ -38,14 +38,15 @@ def today_value(series):
     #print(r["observations"][0]["value"])
 
     today_value = r["observations"][0]["value"]
-    return today_value
+    today_date = r["observations"][0]["date"]
+    return today_value, today_date
 
 
 
 def last_month_value(series):
 
     def buffer(x):
-        x = x - timedelta(days=7)
+        x = x - timedelta(days=5)
         return x
 
     last_month_end_date = datetime.today().replace(day=1)
@@ -69,7 +70,46 @@ def last_month_value(series):
     #print(r["observations"][0]["value"])
 
     last_month_value = r["observations"][0]["value"]
-    return last_month_value
+    last_month_date = r["observations"][0]["date"]
+    return last_month_value, last_month_date
+
+def last_quarter_value(series):
+
+    today = datetime.today()
+    if today.month == 10 or 11 or 12:
+        today = today.replace(month=9,day=30)
+    elif today.month == 7 or 8 or 9:
+        today = today.replace(month=6,day=30)
+    elif today.month == 4 or 5 or 6:
+        today = today.replace(month=3,day=31)
+    elif today.month == 1 or 2 or 3:
+        today = today.replace(month=1, day=1 )
+        today = today - timedelta(days=1)
+
+    def buffer(x):
+        x = x - timedelta(days=7)
+        return x
+
+    last_quater_end_date = today
+    last_quarter_start_date = buffer(last_quater_end_date)
+
+    payload = {
+        "series_id" : series,
+        "api_key" : "fe2f1e3d86308243b33bd5adc4174e6a",
+        "file_type" : "json",
+        "frequency" : "d",
+        "observation_start" : last_quarter_start_date.strftime('%Y-%m-%d'),
+        "observation_end" : last_quater_end_date.strftime('%Y-%m-%d'),
+        "sort_order" : "desc"
+    }
+
+    r = requests.get('https://api.stlouisfed.org/fred/series/observations', params=payload)
+    r = r.json()
+
+    last_quarter_value = r["observations"][0]["value"]
+    last_quarter_date = r["observations"][0]["date"]
+
+    return last_quarter_value, last_quarter_date
 
 
 def last_year_value(series):
@@ -98,7 +138,8 @@ def last_year_value(series):
     #print(r["observations"][0]["value"])
 
     last_year_value = r["observations"][0]["value"]
-    return last_year_value
+    last_year_date = r["observations"][0]["date"]
+    return last_year_value, last_year_date
 
 
 def get_series():
@@ -107,21 +148,25 @@ def get_series():
     "DGS1" : "1Y US Treasury",
     "DGS2" : "2Y US Treasury",
     "DGS5" : "5Y US Treasury",
-    "DGS10" : "10Y US Treasury"
+    "DGS10" : "10Y US Treasury",
+    "DGS30" : "30Y US Treasury"
     }
     return series, series_names
 
 def collect_values():
     series, series_names = get_series()
-    values = [[],[],[],[]]
+    values = [[],[],[],[],[],[]]
     #for i in range(0,len(series)):
     #    values.append([])
     for x in series:
-        todays_values = today_value(x)
+        todays_values, todays_date = today_value(x)
         values[0].append(todays_values)
-        last_months_values = last_month_value(x)
+        last_months_values, last_month_date = last_month_value(x)
         values[1].append(last_months_values)
-        last_years_values = last_year_value(x)
-        values[2].append(last_years_values)
-        values[3].append(series_names[x])
+        last_quarters_value, last_quarters_date = last_quarter_value(x)
+        values[2].append(last_quarters_value)
+        last_years_values, last_years_date = last_year_value(x)
+        values[3].append(last_years_values)
+        values[4].append(series_names[x])
+        values[5] = [todays_date, last_month_date, last_quarters_date, last_years_date]
     return values
